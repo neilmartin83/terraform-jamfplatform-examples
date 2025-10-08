8054a41dcdb344f2d957af71633869e07fb2a8a1
Neil Martin <neil.martin@jamf.com>
2025-10-08 11:58:05 -0600

    Update jamfplatform provider version to 0.1.3 and add software update settings resource

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     jamfplatform = {
       source  = "Jamf-Concepts/jamfplatform"
-      version = "0.1.2"
+      version = "0.1.3"
     }
     jamfpro = {
       source  = "deploymenttheory/jamfpro"
@@ -23,3 +23,37 @@ data "jamfpro_group" "jamfplatform_demo_target_group" {
 }
 
 # Add data source and resource blocks below this line as required
+# Software Update Settings
+resource "jamfplatform_blueprints_blueprint" "software_update_settings" {
+  name        = "Software Update Settings"
+  description = "Managed by Terraform"
+
+  device_groups = [data.jamfpro_group.jamfplatform_demo_target_group.group_platform_id]
+
+  software_update_settings {
+    allow_standard_user_os_updates           = true
+    automatic_download                       = "AlwaysOn"
+    automatic_install_os_updates             = "AlwaysOn"
+    automatic_install_security_updates       = "AlwaysOn"
+    beta_program_enrollment                  = "Allowed"
+    deferral_combined_period_days            = 7
+    deferral_major_period_days               = 30
+    deferral_minor_period_days               = 14
+    deferral_system_period_days              = 3
+    notifications_enabled                    = true
+    rapid_security_response_enabled          = true
+    rapid_security_response_rollback_enabled = false
+    recommended_cadence                      = "Newest"
+
+    # Beta offer programs
+    beta_offer_programs {
+      token       = "beta-token-1"
+      description = "iOS 18 Beta Program"
+    }
+
+    beta_offer_programs {
+      token       = "beta-token-2"
+      description = "macOS Sequoia Beta Program"
+    }
+  }
+}
